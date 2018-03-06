51f7e77cc8485684254bbb4b691378cf3a1b140e
CharlyWelch <blwbiology@gmail.com>
2018-03-06 09:54:41 -0800

    add error and loading handling in App, associated tests

--- src/components/album/Album.js
+++ src/components/album/Album.js
@@ -1,4 +1,29 @@
 import React, { Component } from 'react';
-import Image from '../image/Image';
+// import Image from '../image/Image';
 import { connect } from 'react-redux';
-import { } from './actions';
+import { addAlbum, loadAlbum } from './actions';
+
+class Album extends Component {
+  
+  componentDidMount() {
+    this.props.loadAlbum();
+  }
+
+  render() {
+    const { addAlbum, images } = this.props;
+    return (
+      <div>
+        <ul className="images">
+          {images.map(image => {
+            return <Image key={image.id} {...image}/>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({ images: state.images }),
+  { addAlbum, loadAlbum }
+)(Album);
